8cac414ce9a1046714056adcd87317630de9c001
Sean Maguire <smmaguire@gmail.com>
2020-09-23 12:30:19 -0400

    idk

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Notes
 Location on the server: /mnt/home/smaguire/work/r2c2/r2c2_dd
 
+Conda environment: C3POa (hpc-l1 server)
--- c3poa_mod/launch_c3poa.py
+++ c3poa_mod/launch_c3poa.py
@@ -10,7 +10,7 @@ file=/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_151
 
 new_name=${file#/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode01/}
 new_name=${new_name%.fastq}
-current_dir="/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output/$new_name"
+current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output/$new_name
 mkdir -p $current_dir
 
 qsub -v reads=$file,current_d=$current_dir c3poa.sh
