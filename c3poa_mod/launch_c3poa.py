7272d1853b2bcc4195376bbb11265fb177e558ab
Sean Maguire <smmaguire@gmail.com>
2020-09-23 14:04:07 -0400

    bug fix

--- c3poa_mod/launch_c3poa.py
+++ c3poa_mod/launch_c3poa.py
@@ -8,11 +8,11 @@
 
 data_path=/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode01/
 for i in $( ls $data_path/*.fastq); do
-new_name=${file#$data_path}
+new_name=${i#$data_path}
 new_name=${new_name%.fastq}
 current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output/$new_name
 mkdir -p $current_dir
 
-qsub -v reads=$file,current_d=$current_dir,name=$new_name c3poa.sh
+qsub -v reads=$i,current_d=$current_dir,name=$new_name c3poa.sh
 
 done
