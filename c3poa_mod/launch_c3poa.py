768656e77823431f00d6cd103741804d18b052b5
Sean Maguire <smmaguire@gmail.com>
2020-09-24 09:11:53 -0400

    almost got it

--- c3poa_mod/launch_c3poa.py
+++ c3poa_mod/launch_c3poa.py
@@ -6,13 +6,16 @@
 
 ### For now the file will be just be a test. Want to see how long it takes. 
 
-data_path=/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode02/
+data_path="/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode02/"
 for i in $( ls $data_path/*.fastq); do
 new_name=${i#$data_path}
 new_name=${new_name%.fastq}
-current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output$new_name
+current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output/$new_name
 mkdir -p $current_dir
+echo $current_dir
+echo $new_name
 
-qsub -v reads=$i,current_d=$current_dir,name=$new_name c3poa.sh
+
+#qsub -v reads=$i,current_d=$current_dir,name=$new_name c3poa.sh
 
 done
