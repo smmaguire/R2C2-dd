7bbedceb2895af7814ebbda3090c98f7808d65bb
Sean Maguire <smmaguire@gmail.com>
2020-09-25 11:32:26 -0400

    Run barcode 1 now

--- c3poa_mod/c3poa.sh
+++ c3poa_mod/c3poa.sh
@@ -28,19 +28,19 @@ cd ${current_d}
 ### makeblastdb -in ${name}_Consensus.fasta -parse_seqids -dbtype 'nucl'
 
 ########## Blast the adapter sequence against the blast database
-# blastn \
-# -query ${name}_Consensus.fasta \
-# -subject /mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_mod/resources/reference/reference.fasta \
-# -task "blastn-short" \
-# -word_size 10 \
-# -gapopen 5 \
-# -gapextend 2 \
-# -outfmt 6 \
-# -evalue 1000 \
-# -penalty -3 \
-# -reward 1 \
-# -num_alignments 1 \
-# > ${name}"_aln.tab"
+blastn \
+-query ${name}_Consensus.fasta \
+-subject /mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_mod/resources/reference/reference.fasta \
+-task "blastn-short" \
+-word_size 10 \
+-gapopen 5 \
+-gapextend 2 \
+-outfmt 6 \
+-evalue 1000 \
+-penalty -3 \
+-reward 1 \
+-num_alignments 1 \
+> ${name}"_aln.tab"
 
 Rscript /mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_mod/read_blast_rotate_seq.R ${name}"_aln.tab" ${name}"_Consensus.fasta" ${name}
 
--- c3poa_mod/launch_c3poa.py
+++ c3poa_mod/launch_c3poa.py
@@ -6,7 +6,7 @@
 
 ### For now the file will be just be a test. Want to see how long it takes. 
 
-data_path="/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode02/"
+data_path="/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode01/"
 for i in $( ls $data_path*.fastq); do
 new_name=${i#$data_path}
 new_name=${new_name%.fastq}
