4301e22e09c4f85047e405468327d846149ea9c1
Sean Maguire <smmaguire@gmail.com>
2020-10-26 16:32:58 -0400

    update launch c3poa

--- c3poa_mod/launch_c3poa.py
+++ c3poa_mod/launch_c3poa.py
@@ -6,15 +6,14 @@
 
 ### For now the file will be just be a test. Want to see how long it takes. 
 
-data_path="/mnt/home/smaguire/work/r2c2/r2c2_dd/data/DD_aug_7_blue_noblue/20200807_1510_X5_FAO16912_6c0acd68/fastq_pass/barcode01/"
+data_path="/mnt/home/smaguire/work/r2c2/r2c2_dd/data/Human Brain-Sep18-2020/fastq/fastq_pass/barcode01/"
 for i in $( ls $data_path*.fastq); do
 new_name=${i#$data_path}
 new_name=${new_name%.fastq}
-current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_output/$new_name
+current_dir=/mnt/home/smaguire/work/r2c2/r2c2_dd/c3poa_human_output/$new_name
 mkdir -p $current_dir
 echo $current_dir
 echo $new_name
-
 
 qsub -v reads=$i,current_d=$current_dir,name=$new_name c3poa.sh
 
